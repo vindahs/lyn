566f892ca1a1660d131035be2ee8871258438b9e
vindahs <vincentonoja@ymail.com>
2022-01-05 15:13:35 +0100

    first commit

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+
 
 module.exports = router;
